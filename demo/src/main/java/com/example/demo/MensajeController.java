7b2ab75d3518b7556afd0d312749c9b443d4bc35
Gonzalo <gonzalo.cuevas.m@usach.cl>
2022-04-18 01:32:16 -0400

    Aplicacion agregada

---
+++ demo/src/main/java/com/example/demo/MensajeController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MensajeController {
+	@GetMapping("/mensaje")
+	public String mensaje() {
+		return ("Aplicación web muy simple");
+	}
+}
